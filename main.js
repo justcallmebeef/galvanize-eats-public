860caddec1f8c69283c4822c16899cac40738ca7
justcallmebeef <bethanymitch10@gmail.com>
2018-09-24 16:59:10 -0600

    added js for pulling down dropdown menu

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-});
+document.addEventListener("DOMContentLoaded", function (event) {
+    var foodList = document.querySelector("#foodList")
+    var form = document.querySelector("form")
+
+    fetch("https://galvanize-eats-api.herokuapp.com/menu")
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.menu.length; i++) {
+                var dropdownMenu = document.createElement("option")
+                foodList.appendChild(dropdownMenu)
+                dropdownMenu.innerHTML = data.menu[i].name + " " + "$" + data.menu[i].price
+                console.log(data.menu.length, i)
+            }
+        })
+        .catch(function (error) {
+            console.error(error)
+        })
+})
