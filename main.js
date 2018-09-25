27f02549745ed7f192a233d3ddb32095a5e220ac
justcallmebeef <bethanymitch10@gmail.com>
2018-09-25 14:57:42 -0600

    name of food item appears in order list

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var foodList = document.querySelector("#foodList")
     var form = document.querySelector("form")
+    var orderList = document.querySelector(".orderList")
+    var addButton = document.querySelector("#addButton")
+    var menuSelectionPrice
+    var menuSelection
 
     fetch("https://galvanize-eats-api.herokuapp.com/menu")
         .then(function (response) {
@@ -11,10 +15,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 var dropdownMenu = document.createElement("option")
                 foodList.appendChild(dropdownMenu)
                 dropdownMenu.innerHTML = data.menu[i].name + " " + "$" + data.menu[i].price
-                console.log(data.menu.length, i)
+                dropdownMenu.setAttribute('value', data.menu[i].name)
+                // dropdownMenu.setAttribute('value', data.menu[i].price)
             }
+            return data
+        })
+        .then(function (data) {
+            foodList.addEventListener("change", function selectItem(event) {
+                console.log(event.target)
+                var valueSelected = foodList.value
+            })
+        })
+        .then(function (data) {
+            addButton.addEventListener("click", function addItem(event) {
+                var orderListItem = document.createElement("li")
+                var valueSelected = document.createTextNode(foodList.value)
+                orderList.appendChild(orderListItem)
+                orderListItem.appendChild(valueSelected)
+            })
         })
         .catch(function (error) {
             console.error(error)
-        })
-})
+        });
+});
